afcfc60ad075bd8474bfaad38ce59fe510d658a2
Iliyas Jorio <iliyas@jor.io>
2024-12-21 12:55:34 +0100

    Document rationale for gettext migration

--- gitfourchette/localization.py
+++ gitfourchette/localization.py
@@ -4,6 +4,25 @@
 # For full terms, see the included LICENSE file.
 # -----------------------------------------------------------------------------
 
+# In December 2024, GitFourchette migrated to gettext due to shortcomings
+# in Qt Linguist tooling for Python:
+# - In PyQt6, self.tr() incorrectly returns non-localized text when called
+#   from a subclass of the class that defines the string;
+# - pylupdate6 forces a context in contextless tr() calls.
+# - pyside6-lupdate yields better results, but it isn't readily available on
+#   some Linux distros, causing friction for contributors;
+# - pyside6-lupdate ignores plurals in translate() (explicit context);
+# - pyside6-lupdate -pluralsonly (for English) doesn't play well with Weblate.
+#
+# Benefits of gettext:
+# - It just works. No weird edge cases causing English text to pop up in an
+#   otherwise complete localization;
+# - POEdit or xgettext/msgmerge are easier to install for contributors;
+# - Weblate has great support for gettext;
+# - Custom gettext functions allow for reduced visual noise in the code;
+# - English plurals are defined straight from the code. No need for "actual
+#   English" translations of "developer English" strings such as "%n file(s)".
+
 from gettext import GNUTranslations
 from gettext import NullTranslations
 
