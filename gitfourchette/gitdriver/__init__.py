af6f9dbb09a2899f7bbb3e3afe64f108163e4dab
Iliyas Jorio <iliyas@jor.io>
2025-12-29 18:09:12 +0100

    Create gitdriver package

---
+++ gitfourchette/gitdriver/__init__.py
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------
+# Copyright (C) 2025 Iliyas Jorio.
+# This file is part of GitFourchette, distributed under the GNU GPL v3.
+# For full terms, see the included LICENSE file.
+# -----------------------------------------------------------------------------
+
+from .gitdriver import ABDelta
+from .gitdriver import ABDeltaFile
+from .gitdriver import ConflictSides
+from .gitdriver import GitDriver
+from .gitdriver import VanillaConflict
+from .gitdriver import VanillaFetchStatusFlag
+from .gitdriver import argsIf
