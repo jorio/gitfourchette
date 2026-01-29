5a331beecfcf2904ae16995f7d0ffa1d4244a51b
Iliyas Jorio <iliyas@jor.io>
2026-01-29 20:26:03 +0100

    LFS: Cache old/new LFS info with separate commit/attrcheck

--- gitfourchette/gitdriver/gitdelta.py
+++ gitfourchette/gitdriver/gitdelta.py
@@ -6,9 +6,11 @@
 
 import dataclasses
 
+from pygit2.enums import AttrCheck
+
 from gitfourchette.gitdriver.gitconflict import GitConflict
 from gitfourchette.gitdriver.gitdeltafile import GitDeltaFile, FileMode, NavContext
-from gitfourchette.porcelain import Repo, Oid
+from gitfourchette.porcelain import Repo, Oid, NULL_OID
 
 
 @dataclasses.dataclass
@@ -33,6 +35,37 @@ class GitDelta:
         return FileMode.COMMIT in (self.old.mode, self.new.mode)
 
     def cacheLfsPointers(self, repo: Repo, commitId: Oid):
-        self.old.cacheLfsPointer(repo, commitId)
-        self.new.cacheLfsPointer(repo, commitId)
+        if not (self.old.lfs.state and self.new.lfs.state):
+            self._cacheLfsPointers(repo, commitId)
         return self.old.lfs or self.new.lfs
+
+    def _cacheLfsPointers(self, repo: Repo, newCommitId: Oid):
+        old = self.old
+        new = self.new
+
+        try:
+            oldCommitId = repo[newCommitId].parent_ids[0]
+        except (KeyError, IndexError):
+            oldCommitId = newCommitId
+
+        if oldCommitId != NULL_OID:
+            oldCheck = AttrCheck.INCLUDE_COMMIT
+        elif old.source.isDirty():
+            # TODO: Not 100% sure about this - we need tests!
+            oldCheck = AttrCheck.INDEX_ONLY
+        else:
+            oldCheck = AttrCheck.INDEX_THEN_FILE
+
+        if oldCommitId == NULL_OID and not repo.head_is_unborn:
+            oldCheck |= AttrCheck.INCLUDE_HEAD
+
+        if newCommitId != NULL_OID:
+            newCheck = AttrCheck.INCLUDE_COMMIT
+        elif new.source.isDirty():
+            newCheck = AttrCheck.FILE_THEN_INDEX
+        else:
+            # TODO: Not 100% sure about this - we need tests!
+            newCheck = AttrCheck.INDEX_ONLY
+
+        self.old.cacheLfsPointer(repo, oldCommitId, oldCheck)
+        self.new.cacheLfsPointer(repo, newCommitId, newCheck)
