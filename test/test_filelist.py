b677c1a73365889daecb428059272b5fc35347ba
Zach Rammell <zach@evergreen-games.com>
2026-02-02 23:43:41 -0800

    Unstage both addition and deletion when unstaging rename

--- test/test_filelist.py
+++ test/test_filelist.py
@@ -660,3 +660,33 @@ def testFileListNaturalSort(tempDir, mainWindow):
 
     rw = mainWindow.openRepo(wd)
     assert qlvGetRowData(rw.dirtyFiles) == names
+
+
+def testUnstageRenamedFile(tempDir, mainWindow):
+    wd = unpackRepo(tempDir)
+    writeFile(f"{wd}/a.txt", "content")
+    
+    with RepoContext(wd) as repo:
+        repo.index.add("a.txt")
+        repo.create_commit_on_head("initial", TEST_SIGNATURE, TEST_SIGNATURE)
+
+    os.rename(f"{wd}/a.txt", f"{wd}/b.txt")
+    
+    with RepoContext(wd) as repo:
+        repo.index.add_all()
+        repo.index.write()
+
+    rw = mainWindow.openRepo(wd)
+    
+    staged = list(qlvGetRowData(rw.stagedFiles))
+    assert staged == ["b.txt"]
+    
+    rw.diffArea.stagedFiles.selectAll()
+    triggerContextMenuAction(rw.diffArea.stagedFiles.viewport(), "unstage")
+    
+    rw.refreshRepo()
+    
+    status = rw.repo.status()
+    
+    assert status['a.txt'] == FileStatus.WT_DELETED
+    assert status['b.txt'] == FileStatus.WT_NEW
